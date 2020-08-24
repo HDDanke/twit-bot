65d89072bb79a1632d46d717041346ca1064ed10
HDDanke <70151539+HDDanke@users.noreply.github.com>
2020-08-24 17:30:55 +0300

    Add files via upload

---
+++ cogs/cogs.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+
+class CogCommands(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    # Hidden means it won't show up on the default help.
+    @commands.command(name='load', hidden=True)
+    @commands.is_owner()
+    async def load_cog(self, ctx, *, cog: str):
+        """Command which Loads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.bot.load_extension(cog)
+        except Exception as e:
+            print(e)
+            await ctx.message.add_reaction('❌')
+        else:
+            await ctx.message.add_reaction('✅')
+
+    @commands.command(name='unload', hidden=True)
+    @commands.is_owner()
+    async def unload_cog(self, ctx, *, cog: str):
+        """Command which Unloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.bot.unload_extension(cog)
+        except Exception as e:
+            print(e)
+            await ctx.message.add_reaction('❌')
+        else:
+            await ctx.message.add_reaction('✅')
+
+    @commands.command(name='reload', hidden=True)
+    @commands.is_owner()
+    async def reload_cog(self, ctx, *, cog: str):
+        """Command which Reloads a Module.
+        Remember to use dot path. e.g: cogs.owner"""
+
+        try:
+            self.bot.unload_extension(cog)
+            self.bot.load_extension(cog)
+        except Exception as e:
+            print(e)
+            await ctx.message.add_reaction('❌')
+        else:
+            await ctx.message.add_reaction('✅')
+
+
+def setup(bot):
+    bot.add_cog(CogCommands(bot))
