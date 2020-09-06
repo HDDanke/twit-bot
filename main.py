5b9d177ab062b0c8739603de78cf1b391baf9d0d
HDanke <70151539+HDDanke@users.noreply.github.com>
2020-09-07 02:39:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ with open("token.txt","r") as file:
 
 bot = commands.Bot('-')
 
-extensions = ['cogs.tweet', 'cogs.cogs']
+extensions = ['cogs.tweet', 'cogs.cogs', 'cogs.settings']
 
 if __name__ == '__main__':
     for extension in extensions:
@@ -16,7 +16,12 @@ if __name__ == '__main__':
 async def on_ready():
 
     print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
-    game = discord.Game("with links")
+    game = discord.Game("with links (v3)")
     await bot.change_presence(activity=game)
+
+@bot.command(hidden=True)
+@commands.is_owner()
+async def stop(ctx):
+    await ctx.bot.logout()
 
 bot.run(TOKEN, bot=True, reconnect=True)
